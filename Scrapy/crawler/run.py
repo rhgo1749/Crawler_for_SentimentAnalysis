4b92905cb2a50b6a5e0c4f13b55103b655e5258a
Daehun Go <rhgo1749@naver.com>
2020-09-16 19:04:38 +0900

    Change pipeline.py and Change directory status

--- Scrapy/crawler/run.py
+++
@@ -1,8 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from crawler.pipelines import TextPipeline
-
-process = CrawlerProcess(get_project_settings())
-process.crawl('reviewbot1.py', domain='https://movie.daum.net/moviedb/main?movieId=2')
-process.start() # the script will block here until the crawling is finished
-print(TextPipeline.list_csv)
