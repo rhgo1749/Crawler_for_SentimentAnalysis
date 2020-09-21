544ca64f6909d411035eaf83c20d2a06fba21e1e
Daehun Go <rhgo1749@naver.com>
2020-09-22 06:08:28 +0900

    Modified main.py for modulizaion #2

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from crawler.utils import MovieCrawler
+url = 'https://movie.daum.net/moviedb/main?movieId=1'
 
 if __name__ == '__main__':    # 프로그램의 시작점일 때만 아래 코드 실행
     crawler = MovieCrawler()
-    crawler.crawl('https://movie.daum.net/moviedb/main?movieId=125231')
+    crawler.crawl(url)
